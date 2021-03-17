bae507788620462c0ddbbadceedef93768d2e10b
Francisco Eugênio Wernke <franwernke@usp.br>
2021-03-16 22:06:11 -0300

    Start structuring collecting cycle
    Signed-off-by: Francisco Eugênio Wernke <franwernke@usp.br>

--- src/views/Accept.vue
+++ src/views/Accept.vue
@@ -1,17 +1,45 @@
 <template>
   <v-container>
-      <h1>Você aceita fazer parte da coleta?</h1>
-      <h2>Caso sim, diga "Aceito"</h2>
-      <v-btn>Gravar Aceite</v-btn>
+    <div class="progress-bar">
+      <div class="steps">
+        <strong>Ficha técnica</strong>
+        <strong>Aceite</strong>
+        <strong>Vogal Sustentada</strong>
+        <strong>Parlenda</strong>
+        <strong>Texto Escrito</strong>
+      </div>
+      <v-progress-linear value="20"></v-progress-linear>
+    </div>
+    <h1>Você aceita fazer parte da coleta?</h1>
+    <h2>Caso sim, diga "Aceito"</h2>
+    <Microphone/>
   </v-container>
 </template>
 
 <script>
+import Microphone from '@/components/Microphone'
+
 export default {
-    name: 'Accept',
+  name: 'Accept',
+  components: { Microphone }
 }
 </script>
 
 <style>
-
+  .container {
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+  }
+  .v-timeline {
+    display: flex;
+    flex-direction: column;
+  }
+  .steps {
+    display: flex;
+  }
+  .steps strong {
+    display: flex;
+    place-items: center;
+  }
 </style>

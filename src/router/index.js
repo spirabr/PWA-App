213ccdceda8f74e1c0d6cf0bba7518b8aad7d575
Francisco Eugênio Wernke <franwernke@usp.br>
2021-03-25 22:04:59 -0300

    Add gathering and about routes
    Signed-off-by: Francisco Eugênio Wernke <franwernke@usp.br>

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import TestAudio from '../views/TestAudio.vue'
 import Home from '../views/Home.vue'
-import Accept from '../views/Accept.vue'
+import About from '../views/About.vue'
+import Form from '../views/gather/Form.vue'
+import Accept from '../views/gather/Accept.vue'
+import SusVowel from '../views/gather/SusVowel.vue'
+import Parlend from '../views/gather/Parlend.vue'
+import Phrase from '../views/gather/Phrase.vue'
+import Done from '../views/gather/Done.vue'
 
 Vue.use(VueRouter)
 
@@ -12,9 +19,44 @@ const routes = [
     component: Home,
   },
   {
-    path: '/accept',
+    path: '/about',
+    name: 'About',
+    component: About,
+  },
+  {
+    path: '/gather/form',
+    name: 'Form',
+    component: Form
+  },
+  {
+    path: '/gather/accept',
     name: 'Accept',
     component: Accept
+  },
+  {
+    path: '/gather/susvowel',
+    name: 'SusVowel',
+    component: SusVowel
+  },
+  {
+    path: '/gather/parlend',
+    name: 'Parlend',
+    component: Parlend
+  },
+  {
+    path: '/gather/phrase',
+    name: 'Phrase',
+    component: Phrase
+  },
+  {
+    path: '/gather/done',
+    name: 'Done',
+    component: Done
+  },
+  {
+    path: '/test',
+    name: 'TestAudio',
+    component: TestAudio
   }
 ]
 

defaca2c7910a4ace68d034faf1d211cd1dad5cd
Francisco Eugênio Wernke <franwernke@usp.br>
2021-03-29 16:04:15 -0300

    Add global style to App.vue
    Signed-off-by: Francisco Eugênio Wernke <franwernke@usp.br>

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,13 @@ export default {
   name: 'App',
 }
 </script>
+
+<style>
+  .container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+  }
+</style>

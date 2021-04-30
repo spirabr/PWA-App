759cef464934be5e3e27a56f2688454a36bc2226
RafaelDoering <rafael.doering.soares@usp.br>
2021-04-30 20:06:02 -0300

    Fix worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,9 +1,6 @@
 if (workbox) {
-    // adjust log level for displaying workbox logs
-    workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug)
-
     // apply precaching. In the built version, the precacheManifest will
-    // be imported using importScripts (as is workbox itself) and we can 
+    // be imported using importScripts (as is workbox itself) and we can
     // precache this. This is all we need for precaching
     workbox.precaching.precacheAndRoute(self.__precacheManifest);
 }

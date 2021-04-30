b0803783389c3739d801b200c99271e65c473550
RafaelDoering <rafael.doering.soares@usp.br>
2021-04-30 20:15:41 -0300

    Add index.html to cache

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,4 +3,5 @@ if (workbox) {
     // be imported using importScripts (as is workbox itself) and we can
     // precache this. This is all we need for precaching
     // workbox.precaching.precacheAndRoute(self.__precacheManifest);
+    workbox.routing.registerNavigationRoute('/index.html');
 }

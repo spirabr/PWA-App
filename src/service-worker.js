a6f0fa93f169e5ecb15a4bda377a3e4a2b46402b
RafaelDoering <rafael.doering.soares@usp.br>
2021-04-30 20:11:26 -0300

    Remove service-worker prechaching

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,5 +2,5 @@ if (workbox) {
     // apply precaching. In the built version, the precacheManifest will
     // be imported using importScripts (as is workbox itself) and we can
     // precache this. This is all we need for precaching
-    workbox.precaching.precacheAndRoute(self.__precacheManifest);
+    // workbox.precaching.precacheAndRoute(self.__precacheManifest);
 }

95f1bf1a2ba43b04d9deada966f9d2386236a194
Francisco Eugênio Wernke <franwernke@usp.br>
2021-05-31 23:11:51 -0300

    Add new TCLE compliance term
    Signed-off-by: Francisco Eugênio Wernke <franwernke@usp.br>

--- src/views/gather/Aceite.vue
+++ src/views/gather/Aceite.vue
@@ -3,13 +3,18 @@
     <div class="instruction">
       <h1>termo de participação</h1>
       <p>
-        Você está sendo convidado a participar de um estudo para desenvolver um método automático para detectar pessoas que possam evoluir com problemas respiratórios.
-
-        Caso você concorde iremos pedir para que você grave uma ou mais frases pelo computador.
-
-        Os dados de sua voz serão analisados por pesquisadores da Universidade de São Paulo de forma anônima.
-                  
-        Nenhum dado pessoal seu será incluído em nenhum documento do estudo.
+        Você está sendo convidado/a participar, como voluntário/a, de um estudo que desenvolve 
+        um método automático de detecção de características da voz e da fala. 
+        Esse método será de grande relevância para diagnósticos médicos. 
+        Maiores detalhes sobre isso poderão ser dados logo na sequência à sua participação. 
+        Sendo sua participação voluntária, você poderá desistir da coleta a qualquer momento.
+        
+        Caso você concorde, iniciaremos a gravação da sua fala. Para isso, é só seguir as instruções
+        da coletora ou do coletor. Os dados da sua voz e de sua fala serão analisados, de forma 
+        anônima, por pesquisadores da Universidade de São Paulo. Todos os seus dados pessoais 
+        estarão anônimos em todos os documentos do estudo.
+        
+        Você aceita participar do estudo?
       </p>
     </div>
 
